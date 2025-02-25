d40ea5c93c38471d496f3ccf1d18d73f9c65a8bc
T <evanniaraja@dovercorp.com>
2025-02-25 12:51:17 +0530

    added script in thank you page

--- src/app/thankyou/page.tsx
+++ src/app/thankyou/page.tsx
@@ -5,6 +5,10 @@ export default function Page() {
     <div className="bg-gradient-to-r from-blue-700 to-fuchsia-500 flex items-center justify-center h-screen">
       <div className="grid grid-rows-2 gap-5 text-fuchsia-100">
         <div className="text-6xl text-center">Thank You for Contacting Us!</div>
+        <script
+          src="https://assets.adobedtm.com/59b4ab82adc6/48200609e90b/launch-85f0fd1a20f5-development.min.js"
+          async
+        ></script>
       </div>
     </div>
   );
